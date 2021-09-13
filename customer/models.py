b80ea84cc04e2b18e69f0d6daf86f12495666105
AliGachpazha <gachpazha2001@gmail.com>
2021-09-13 12:02:52 +0430

    add login_user

--- customer/models.py
+++ customer/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class User(AbstractBaseUser)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=60)
